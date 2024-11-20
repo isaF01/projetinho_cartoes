bc076714caec8d37921bae0ff61c0edd22df38ec
Isabelly <0000111401154Xsp@al.educacao.sp.gov.br>
2024-11-20 19:55:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é Python?', 
+    'O Python é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?', 
+    'A capital da França é Paris.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?', 
+    'Uma função é um bloco de código que executa alguma tarefa.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em ingês?', 
+    'Oi em inglês é HI (RAY).'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro imperador do Brasil?', 
+    'Dom Pedro I.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o planeta mais próximo do Sol?', 
+    'Mercúrio.'
+)
+
+criaCartao(
+    'Esportes',
+    'Em que ano o Brasil conquistou sua primeira Copa do Mundo de Futebol?', 
+    '1958.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual é o animal mais rápido do mundo?', 
+    ' O falcão-peregrino, que pode atingir velocidades de até 389 km/h em um mergulho.'
+)
+
+criaCartao(
+    'Entretenimento',
+    'Qual é o nome do brinquedo cowboy em Toy Story?',
+    'Woody.'
+)
